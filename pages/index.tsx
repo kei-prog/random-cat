68c5860385c49697759b78be2e768771faf98e60
kei-prog <kei08prog@gmail.com>
2025-01-27 16:14:53 +0900

    feat getServerSideProps

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,14 @@
-import { NextPage } from "next";
-import { useEffect, useState } from "react";
+import { GetServerSideProps, NextPage } from "next";
+import { useState } from "react";
+// getServerSidePropsから渡されるpropsの型
+type Props = {
+  initialImageUrl: string;
+};
 
-const IndexPage: NextPage = () => {
-  const [imageUrl, setImageUrl] = useState("");
-  const [loading, setLoading] = useState(true);
-  useEffect(() => {
-    fetchImage().then((newImage) => {
-      setImageUrl(newImage.url);
-      setLoading(false);
-    });
-  }, []);
+// ページコンポーネント関数にpropsを受け取る引数を追加する
+const IndexPage: NextPage<Props> = ({ initialImageUrl }) => {
+  const [imageUrl, setImageUrl] = useState(initialImageUrl); // 初期値を渡す
+  const [loading, setLoading] = useState(false); // 初期状態はfalseにしておく
 
   const handleClick = async () => {
     setLoading(true);
@@ -17,7 +16,6 @@ const IndexPage: NextPage = () => {
     setImageUrl(newImage.url);
     setLoading(false);
   };
-
   return (
     <div>
       <button onClick={handleClick}>他のにゃんこも見る</button>
@@ -26,11 +24,18 @@ const IndexPage: NextPage = () => {
   );
 };
 export default IndexPage;
-
+// サーバーサイドで実行する処理
+export const getServerSideProps: GetServerSideProps<Props> = async () => {
+  const image = await fetchImage();
+  return {
+    props: {
+      initialImageUrl: image.url,
+    },
+  };
+};
 type Image = {
   url: string;
 };
-
 const fetchImage = async (): Promise<Image> => {
   const res = await fetch("https://api.thecatapi.com/v1/images/search");
   const images = await res.json();
